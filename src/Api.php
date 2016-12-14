72d73e114f08a281af0c0877500fe2cbb1048c54
Robert Helgesson <robert@chaitsa.com>
2016-12-14 14:06:27 +0000

    Add a little documentation

--- src/Api.php
+++ src/Api.php
@@ -238,15 +238,42 @@ class BatchFilter
 
 }
 
+/**
+ * A page of elements.
+ *
+ * The element type depends on the type of page that has been
+ * retrieved. Typically it is one of `MtSmsBatchResponse` or
+ * `GroupResponse`.
+ */
 class Page implements \IteratorAggregate
 {
 
+    /**
+     * The page number, starting from zero.
+     *
+     * @var int this page's number
+     */
     public $page;
 
+    /**
+     * The number of elements on this page.
+     *
+     * @var int the number of page elements
+     */
     public $size;
 
+    /**
+     * The total number of elements across all fetched pages.
+     *
+     * @var int the total number of elements
+     */
     public $totalSize;
 
+    /**
+     * The page elements.
+     *
+     * @var mixed[] the page elements
+     */
     public $content;
 
     /**
