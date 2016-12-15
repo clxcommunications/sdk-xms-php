48e48bc9dba454fc6e51ab6febf07af5e8a8d5df
Robert Helgesson <robert@chaitsa.com>
2016-12-15 15:15:42 +0000

    Expand tests

--- tests/DeserializeTest.php
+++ tests/DeserializeTest.php
@@ -21,11 +21,20 @@ class DeserializeTest extends PHPUnit\Framework\TestCase
     {
         $json = <<<'EOD'
 {
-    "body": "Hello",
+    "body": "${foo}${bar}",
     "canceled": true,
+    "parameters": {
+        "foo": {
+            "123456789": "Joe",
+            "987654321": "Mary",
+            "default": "you"
+        },
+        "bar": {}
+    },
     "created_at": "2016-12-01T11:03:13.192Z",
     "delivery_report": "none",
-    "expire_at": "2016-12-04T11:03:13.192Z",
+    "send_at": "2016-12-02T11:03:13.192Z",
+    "expire_at": "2016-12-05T11:03:13.192Z",
     "from": "12345",
     "id": "3SD49KIOW8lL1Z5E",
     "modified_at": "2016-12-01T11:03:13Z",
@@ -33,6 +42,7 @@ class DeserializeTest extends PHPUnit\Framework\TestCase
         "987654321",
         "555555555"
     ],
+    "callback_url": "https://example.com/callbacker",
     "type": "mt_text"
 }
 EOD;
@@ -40,24 +50,58 @@ EOD;
         $result = X\Deserialize::batchResponse($json);
 
         $this->assertInstanceOf(X\MtTextSmsBatchResponse::class, $result);
-        $this->assertEquals('Hello', $result->body);
+        $this->assertEquals('${foo}${bar}', $result->body);
         $this->assertTrue($result->canceled);
         $this->assertEquals(
             new DateTime('2016-12-01T11:03:13.192Z'), $result->createdAt
         );
         $this->assertEquals('none', $result->deliveryReport);
         $this->assertEquals(
-            new DateTime('2016-12-04T11:03:13.192Z'), $result->expireAt
+            new DateTime('2016-12-02T11:03:13.192Z'), $result->sendAt
+        );
+        $this->assertEquals(
+            new DateTime('2016-12-05T11:03:13.192Z'), $result->expireAt
         );
         $this->assertEquals('12345', $result->sender);
         $this->assertEquals('3SD49KIOW8lL1Z5E', $result->batchId);
         $this->assertEquals(
             new DateTime('2016-12-01T11:03:13Z'), $result->modifiedAt
         );
+        $this->assertEquals(
+            'https://example.com/callbacker', $result->callbackUrl
+        );
         $this->assertEquals(['987654321', '555555555'], $result->recipients);
+        $this->assertEquals(
+            [
+                'foo' => [
+                    'default' => 'you',
+                    '987654321' => 'Mary',
+                    '123456789' => 'Joe'
+                ],
+                'bar' => []
+            ],
+            $result->parameters
+        );
 
         // The type attribute should not be deserialized.
         $this->assertObjectNotHasAttribute('type', $result);
+    }
+
+    public function testReadBatchResponseUnknown()
+    {
+        $json = <<<'EOD'
+{
+    "some_field": "some_value",
+    "type": "mt_what"
+}
+EOD;
+
+        try {
+            $result = X\Deserialize::batchResponse($json);
+            $this->assertTrue(false, "expected exception");
+        } catch (X\UnexpectedResponseException $ex) {
+            // Expected
+        }
     }
 
     public function testReadBatchesPage()
@@ -193,6 +237,59 @@ EOD;
         $this->assertObjectNotHasAttribute('type', $result);
     }
 
+    public function testReadDeliveryReportFull()
+    {
+        $json = <<<'EOD'
+{
+  "type" : "delivery_report_sms",
+  "batch_id" : "4G4OmwztSJbVL2bl",
+  "total_message_count" : 2,
+  "statuses" : [ {
+    "code" : 0,
+    "status" : "Delivered",
+    "count" : 1,
+    "recipients" : [ "555555555" ]
+  }, {
+    "code" : 11,
+    "status" : "Failed",
+    "count" : 1,
+    "recipients" : [ "987654321" ]
+  } ]
+}
+EOD;
+
+        $result = X\Deserialize::batchDeliveryReport($json);
+
+        $this->assertEquals('4G4OmwztSJbVL2bl', $result->batchId);
+        $this->assertEquals(2, $result->totalMessageCount);
+        $this->assertCount(2, $result->statuses);
+
+        $this->assertEquals(0, $result->statuses[0]->code);
+        $this->assertEquals(11, $result->statuses[1]->code);
+
+        $this->assertEquals('Delivered', $result->statuses[0]->status);
+        $this->assertEquals('Failed', $result->statuses[1]->status);
+
+        $this->assertEquals(1, $result->statuses[0]->count);
+        $this->assertEquals(1, $result->statuses[1]->count);
+
+        $this->assertEquals(['555555555'], $result->statuses[0]->recipients);
+        $this->assertEquals(['987654321'], $result->statuses[1]->recipients);
+    }
+
+    public function testReadDeliveryReportUnknownType()
+    {
+        $json = '{ "hello" : "value" }';
+
+        try {
+            X\Deserialize::batchDeliveryReport($json);
+            assertTrue(false, "expected exception");
+        } catch (X\UnexpectedResponseException $ex) {
+            $this->assertEquals($json, $ex->getHttpBody());
+        }
+    }
+
+
     public function testReadGroupResponse()
     {
         $json = <<<'EOD'
@@ -200,10 +297,12 @@ EOD;
     "auto_update": {
         "to": "12345",
         "add": {
-            "first_word": "hello"
+            "first_word": "hello",
+            "second_word": "world"
         },
         "remove": {
-            "first_word": "goodbye"
+            "first_word": "goodbye",
+            "second_word": "world"
         }
     },
     "child_groups": [],
@@ -219,9 +318,9 @@ EOD;
 
         $this->assertEquals('12345', $result->autoUpdate->recipient);
         $this->assertEquals('hello', $result->autoUpdate->addFirstWord);
-        $this->assertNull($result->autoUpdate->addSecondWord);
+        $this->assertEquals('world', $result->autoUpdate->addSecondWord);
         $this->assertEquals('goodbye', $result->autoUpdate->removeFirstWord);
-        $this->assertNull($result->autoUpdate->removeSecondWord);
+        $this->assertEquals('world', $result->autoUpdate->removeSecondWord);
         $this->assertCount(0, $result->childGroups);
         $this->assertEquals(
             new \DateTime('2016-12-08T12:38:19.962Z'),
