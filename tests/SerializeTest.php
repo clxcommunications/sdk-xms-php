ed9f6e6998714c5d3437c40f1c62caaa47a22d56
Robert Helgesson <robert@chaitsa.com>
2016-12-15 09:29:55 +0000

    Add tests for the Serialize class

---
+++ tests/SerializeTest.php
@@ -0,0 +1,90 @@
+<?php
+
+/**
+ * Contains tests of the Serialize class.
+ *
+ * PHP versions 5 and 7
+ *
+ * @license http://www.apache.org/licenses/LICENSE-2.0  Apache License 2.0
+ */
+
+use Clx\Xms as X;
+
+class SerializeTest extends PHPUnit\Framework\TestCase
+{
+
+    public function testBatchCreateText()
+    {
+        $batch = new X\MtTextSmsBatchCreate();
+        $batch->sender = '12345';
+        $batch->recipients = ['987654321', '123456789'];
+        $batch->body = 'Hello, ${name}!';
+        $batch->parameters['name'] = [
+            '987654321' => 'Mary',
+            '123456789' => 'Joe',
+            'default' => 'you'
+        ];
+        $batch->deliveryReport = X\ReportType::NONE;
+        $batch->expireAt = new \DateTime('2016-12-04T11:03:13.192Z');
+
+        $actual = X\Serialize::textBatch($batch);
+
+        $expected = <<<'EOD'
+{
+    "body": "Hello, ${name}!",
+    "delivery_report": "none",
+    "expire_at": "2016-12-04T11:03:13+00:00",
+    "from": "12345",
+    "to": [
+        "987654321",
+        "123456789"
+    ],
+    "parameters": {
+        "name": {
+            "987654321": "Mary",
+            "123456789": "Joe",
+            "default": "you"
+        }
+    },
+    "type": "mt_text"
+}
+EOD;
+
+        $this->assertJsonStringEqualsJsonString($expected, $actual);
+    }
+
+    public function testBatchCreateBinary()
+    {
+        $batch = new X\MtBinarySmsBatchCreate();
+        $batch->sender = '12345';
+        $batch->recipients = ['987654321', '123456789'];
+        $batch->body = "\x00\x01\x02\x03";
+        $batch->udh = "\xff\xfe\xfd";
+        $batch->deliveryReport = X\ReportType::SUMMARY;
+        $batch->expireAt = new \DateTime('2016-12-17T08:15:29.969Z');
+        $batch->tags = [ "tag1", "таг2" ];
+
+        $actual = X\Serialize::binaryBatch($batch);
+
+        $expected = <<<'EOD'
+{
+    "body": "AAECAw==",
+    "delivery_report": "summary",
+    "expire_at": "2016-12-17T08:15:29+00:00",
+    "from": "12345",
+    "tags": [ "tag1", "таг2" ],
+    "to": [
+        "987654321",
+        "123456789"
+    ],
+    "type": "mt_binary",
+    "udh": "fffefd"
+}
+EOD;
+
+        $this->assertJsonStringEqualsJsonString($expected, $actual);
+    }
+
+}
+
+?>
