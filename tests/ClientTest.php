8906445dceaeeae02184fb4e588bfb1c577caf83
Robert Helgesson <robert@chaitsa.com>
2016-12-15 15:51:12 +0000

    Expand client test suite

--- tests/ClientTest.php
+++ tests/ClientTest.php
@@ -139,6 +139,256 @@ class ClientTest extends PHPUnit\Framework\TestCase
         }
     }
 
+    public function testCreateTextBatch()
+    {
+        $responseBody = <<<'EOD'
+{
+  "type" : "mt_text",
+  "body" : "hello",
+  "id" : "5Z8QsIRsk86f-jHB",
+  "to" : [ "987654321", "123456789" ],
+  "from" : "12345",
+  "expire_at" : "2016-12-17T08:15:29.969Z",
+  "created_at" : "2016-12-14T08:15:29.969Z",
+  "modified_at" : "2016-12-14T08:15:29.969Z",
+  "canceled" : false
+}
+EOD;
+
+        $this->http->mock
+            ->when()
+            ->methodIs('POST')
+            ->pathIs('/xms/v1/batches')
+            ->then()
+            ->statusCode(Response::HTTP_CREATED)
+            ->header('content-type', 'application/json')
+            ->body($responseBody)
+            ->end();
+        $this->http->setUp();
+
+        $batch = new X\MtTextSmsBatchCreate();
+        $batch->body = 'hello';
+        $batch->recipients = ['987654321', '123456789'];
+        $batch->sender = '12345';
+
+        $result = $this->_client->createTextBatch($batch);
+
+        $this->assertEquals('5Z8QsIRsk86f-jHB', $result->batchId);
+
+        $expectedRequestBody = <<<'EOD'
+{
+    "type": "mt_text",
+    "body": "hello",
+    "from": "12345",
+    "to": ["987654321", "123456789"]
+}
+EOD;
+
+        $this->assertJsonStringEqualsJsonString(
+            $expectedRequestBody,
+            (string) $this->http->requests->latest()->getBody()
+        );
+    }
+
+    public function testCreateBinaryBatch()
+    {
+        $responseBody = <<<'EOD'
+{
+  "type" : "mt_binary",
+  "udh" : "fffefd",
+  "body" : "AAECAw==",
+  "id" : "5Z8QsIRsk86f-jHB",
+  "to" : [ "987654321", "123456789" ],
+  "from" : "12345",
+  "expire_at" : "2016-12-17T08:15:29.969Z",
+  "created_at" : "2016-12-14T08:15:29.969Z",
+  "modified_at" : "2016-12-14T08:15:29.969Z",
+  "canceled" : false
+}
+EOD;
+
+        $this->http->mock
+            ->when()
+            ->methodIs('POST')
+            ->pathIs('/xms/v1/batches')
+            ->then()
+            ->statusCode(Response::HTTP_CREATED)
+            ->header('content-type', 'application/json')
+            ->body($responseBody)
+            ->end();
+        $this->http->setUp();
+
+        $batch = new X\MtBinarySmsBatchCreate();
+        $batch->body = "\x00\x01\x02\x03";
+        $batch->udh = "\xff\xfe\xfd";
+        $batch->recipients = ['987654321', '123456789'];
+        $batch->sender = '12345';
+
+        $result = $this->_client->createBinaryBatch($batch);
+
+        $this->assertEquals('5Z8QsIRsk86f-jHB', $result->batchId);
+
+        $expectedRequestBody = <<<'EOD'
+{
+  "type" : "mt_binary",
+  "udh" : "fffefd",
+  "body" : "AAECAw==",
+  "to" : [ "987654321", "123456789" ],
+  "from" : "12345"
+}
+EOD;
+
+        $this->assertJsonStringEqualsJsonString(
+            $expectedRequestBody,
+            (string) $this->http->requests->latest()->getBody()
+        );
+    }
+
+    public function testFetchBinaryBatch()
+    {
+        $responseBody = <<<'EOD'
+{
+  "type" : "mt_binary",
+  "udh" : "fffefd",
+  "body" : "AAECAw==",
+  "id" : "5Z8QsIRsk86f-jHB",
+  "to" : [ "987654321", "123456789" ],
+  "from" : "12345",
+  "expire_at" : "2016-12-17T08:15:29.969Z",
+  "created_at" : "2016-12-14T08:15:29.969Z",
+  "modified_at" : "2016-12-14T08:15:29.969Z",
+  "canceled" : false
+}
+EOD;
+
+        $this->http->mock
+            ->when()
+            ->methodIs('GET')
+            ->pathIs('/xms/v1/batches/5Z8QsIRsk86f-jHB')
+            ->then()
+            ->statusCode(Response::HTTP_OK)
+            ->header('content-type', 'application/json')
+            ->body($responseBody)
+            ->end();
+        $this->http->setUp();
+
+        $result = $this->_client->fetchBatch('5Z8QsIRsk86f-jHB');
+
+        $this->assertInstanceOf(X\MtBinarySmsBatchResponse::class, $result);
+        $this->assertEquals('5Z8QsIRsk86f-jHB', $result->batchId);
+    }
+
+    public function testFetchBatches()
+    {
+        $responseBody1 = <<<'EOD'
+{
+    "batches": [
+        {
+            "body": "AAECAw==",
+            "canceled": false,
+            "created_at": "2016-12-14T08:15:29.969Z",
+            "delivery_report": "none",
+            "expire_at": "2016-12-17T08:15:29.969Z",
+            "from": "12345",
+            "id": "5Z8QsIRsk86f-jHB",
+            "modified_at": "2016-12-14T08:15:29.969Z",
+            "tags": [
+                "rah"
+            ],
+            "to": [
+                "987654321",
+                "123456789"
+            ],
+            "type": "mt_binary",
+            "udh": "fffefd"
+        },
+        {
+            "body": "Hello, world!",
+            "canceled": false,
+            "created_at": "2016-12-09T12:54:28.247Z",
+            "delivery_report": "none",
+            "expire_at": "2016-12-12T12:54:28.247Z",
+            "from": "12345",
+            "id": "4nQCc1T6Dg-R-zHX",
+            "modified_at": "2016-12-09T12:54:28.247Z",
+            "tags": [
+                "rah"
+            ],
+            "to": [
+                "987654321"
+            ],
+            "type": "mt_text"
+        },
+        {
+            "body": "Hello",
+            "canceled": false,
+            "created_at": "2016-12-06T11:14:37.438Z",
+            "delivery_report": "none",
+            "expire_at": "2016-12-09T11:14:37.438Z",
+            "from": "12345",
+            "id": "4G4OmwztSJbVL2bl",
+            "modified_at": "2016-12-06T11:14:37.438Z",
+            "tags": [
+                "rah1",
+                "rah2"
+            ],
+            "to": [
+                "987654321",
+                "555555555"
+            ],
+            "type": "mt_text"
+        }
+    ],
+    "count": 7,
+    "page": 0,
+    "page_size": 3
+}
+EOD;
+
+        $responseBody2 = <<<'EOD'
+{
+    "batches": [],
+    "count": 7,
+    "page": 1,
+    "page_size": 0
+}
+EOD;
+
+        $this->http->mock
+            ->when()
+            ->methodIs('GET')
+            ->pathIs('/xms/v1/batches?page=0')
+            ->then()
+            ->statusCode(Response::HTTP_OK)
+            ->header('content-type', 'application/json')
+            ->body($responseBody1)
+            ->end();
+        $this->http->mock
+            ->when()
+            ->methodIs('GET')
+            ->pathIs('/xms/v1/batches?page=1')
+            ->then()
+            ->statusCode(Response::HTTP_OK)
+            ->header('content-type', 'application/json')
+            ->body($responseBody2)
+            ->end();
+        $this->http->setUp();
+
+        $pages = $this->_client->fetchBatches();
+
+        $page = $pages->get(0);
+        $this->assertInstanceOf(X\Page::class, $page);
+        $this->assertEquals(3, $page->size);
+        $this->assertEquals(7, $page->totalSize);
+        $this->assertEquals('4G4OmwztSJbVL2bl', $page->content[2]->batchId);
+
+        $page = $pages->get(1);
+        $this->assertInstanceOf(X\Page::class, $page);
+        $this->assertEquals(0, $page->size);
+        $this->assertEquals(7, $page->totalSize);
+        $this->assertEquals([], $page->content);
+    }
+
 }
 
 ?>
