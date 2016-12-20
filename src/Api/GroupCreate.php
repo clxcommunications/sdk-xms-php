115ffec17db28fcd38a9f83706ceaa7a6b492267
Robert Helgesson <robert@chaitsa.com>
2016-12-20 11:38:05 +0000

    Add file header

--- src/Api/GroupCreate.php
+++ src/Api/GroupCreate.php
@@ -1,5 +1,13 @@
 <?php
 
+/**
+ * Contains the class holding the group create parameters.
+ *
+ * PHP versions 5 and 7
+ *
+ * @license http://www.apache.org/licenses/LICENSE-2.0  Apache License 2.0
+ */
+
 namespace Clx\Xms\Api;
 
 /**
